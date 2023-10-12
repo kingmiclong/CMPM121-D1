d6693ada16e93501beee7fb8fca4d75808fbd727
kingmiclong <b302057069@gmail.com>
2023-10-12 11:17:33 -0700

    Step 4 completed

--- src/main.ts
+++ src/main.ts
@@ -21,24 +21,30 @@ let counter: number = 0; // Initialize counter
 counterDiv.innerHTML = `${counter} rockets`; // Display initial counter value
 app.append(counterDiv);
 
+// Step 4
+let lastTime: number = 0; // Time from last frame
+
 // counter for auto click
-const updateCounter = () => {
-  counter++;
-  counterDiv.innerHTML = `${counter} rockets`;
+const updateCounter = (time: number) => {
+  const deltaTime = time - lastTime; // Time elapsed since last frame in milliseconds
+  const increment = deltaTime * 0.001; // Calculate fractional increment based on time elapsed
+  counter += increment; // Increment counter
+  counterDiv.innerHTML = `${counter.toFixed(2)} rockets`; // Update display with two decimal places
+  lastTime = time; // Update lastTime for the next frame
+  requestAnimationFrame(updateCounter); // Request the next animation frame
 };
+
+// Initialize the animation loop
+requestAnimationFrame(updateCounter);
 
 // Visual respond
 button.addEventListener("click", () => {
   button.style.opacity = "0.5";
-
   // plus 1 each time it click
   counter++;
   counterDiv.innerHTML = `${counter} rockets`;
 
-  setTimeout(() => {
+  requestAnimationFrame(() => {
     button.style.opacity = "1";
-  }, 100);
+  });
 });
-
-// Increment the counter automatically every second
-setInterval(updateCounter, 1000);
