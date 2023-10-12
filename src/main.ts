1db31a1069049f84eaf0827b572a02d47fd26f24
kingmiclong <b302057069@gmail.com>
2023-10-12 11:45:49 -0700

    push Step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -65,3 +65,93 @@ upgradeButton.addEventListener("click", () => {
   growthRate++; // Increment the growth rate by 1
   counterDiv.innerHTML = `${counter.toFixed(2)} rockets`; // Update display
 });
+
+// step 6
+const upgradeDiv = document.createElement("div");
+upgradeDiv.className = "upgrades";
+app.append(upgradeDiv);
+
+// Item A
+let itemACount: number = 0;
+const itemACost: number = 10;
+const itemAGrowthRate: number = 0.1;
+const itemAButton = document.createElement("button");
+itemAButton.innerHTML = `Buy A (${itemACost})`;
+upgradeDiv.append(itemAButton);
+
+// Item B
+let itemBCount: number = 0;
+const itemBCost: number = 100;
+const itemBGrowthRate: number = 2.0;
+const itemBButton = document.createElement("button");
+itemBButton.innerHTML = `Buy B (${itemBCost})`;
+upgradeDiv.append(itemBButton);
+
+// Item C
+let itemCCount: number = 0;
+const itemCCost: number = 1000;
+const itemCGrowthRate: number = 50.0;
+const itemCButton = document.createElement("button");
+itemCButton.innerHTML = `Buy C (${itemCCost})`;
+upgradeDiv.append(itemCButton);
+
+// Status displays
+const statusDiv = document.createElement("div");
+statusDiv.className = "status";
+app.append(statusDiv);
+
+const growthRateDisplay = document.createElement("div");
+growthRateDisplay.innerHTML = `Growth Rate: ${growthRate.toFixed(2)} units/sec`;
+statusDiv.append(growthRateDisplay);
+
+const itemACountDisplay = document.createElement("div");
+itemACountDisplay.innerHTML = `Item A: ${itemACount}`;
+statusDiv.append(itemACountDisplay);
+
+const itemBCountDisplay = document.createElement("div");
+itemBCountDisplay.innerHTML = `Item B: ${itemBCount}`;
+statusDiv.append(itemBCountDisplay);
+
+const itemCCountDisplay = document.createElement("div");
+itemCCountDisplay.innerHTML = `Item C: ${itemCCount}`;
+statusDiv.append(itemCCountDisplay);
+
+// Update function to handle growth and display
+const update = () => {
+  counter += growthRate;
+  counterDiv.innerHTML = `${counter.toFixed(2)} units`;
+  growthRateDisplay.innerHTML = `Growth Rate: ${growthRate.toFixed(
+    2,
+  )} units/sec`;
+  itemACountDisplay.innerHTML = `Item A: ${itemACount}`;
+  itemBCountDisplay.innerHTML = `Item B: ${itemBCount}`;
+  itemCCountDisplay.innerHTML = `Item C: ${itemCCount}`;
+};
+
+// Main update loop
+setInterval(update, 1000);
+
+// Button click handlers
+itemAButton.addEventListener("click", () => {
+  if (counter >= itemACost) {
+    counter -= itemACost;
+    itemACount++;
+    growthRate += itemAGrowthRate;
+  }
+});
+
+itemBButton.addEventListener("click", () => {
+  if (counter >= itemBCost) {
+    counter -= itemBCost;
+    itemBCount++;
+    growthRate += itemBGrowthRate;
+  }
+});
+
+itemCButton.addEventListener("click", () => {
+  if (counter >= itemCCost) {
+    counter -= itemCCost;
+    itemCCount++;
+    growthRate += itemCGrowthRate;
+  }
+});
