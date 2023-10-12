bf093bb2cedbcc20a90688613b576aa1dbcce8b5
kingmiclong <b302057069@gmail.com>
2023-10-12 10:25:50 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,19 @@ const button = document.createElement("button");
 button.innerHTML = "🚀";  
 app.append(button);
 
+// Step 2 
+const counterDiv = document.createElement("div");
+let counter: number = 0;  // Initialize counter
+counterDiv.innerHTML = `${counter} rockets`;  // Display initial counter value
+app.append(counterDiv);
+
 // Visual respond
 button.addEventListener("click", () => {
   button.style.opacity = "0.5";  
   
+  // plus 1 each time it click
+  counter++;
+  counterDiv.innerHTML = `${counter} rockets`;
   
   setTimeout(() => {
     button.style.opacity = "1";
