928751532e30771acd5f0b60051e7f9cfd1ea55d
kingmiclong <b302057069@gmail.com>
2023-10-12 09:57:20 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,18 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Step 1 
+const button = document.createElement("button");
+button.innerHTML = "🚀";  
+app.append(button);
+
+// Visual respond
+button.addEventListener("click", () => {
+  button.style.opacity = "0.5";  
+  
+  
+  setTimeout(() => {
+    button.style.opacity = "1";
+  }, 100);
+});
